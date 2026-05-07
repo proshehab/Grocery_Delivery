0d28e1b3fea79e349f30e81e7a5ed84d5a45203a
Shehab Bin  Shakib <96295133+proshehab@users.noreply.github.com>
2026-05-07 22:03:17 +0600

    refactor: update App component structure and main rendering method

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import { Toaster } from "react-hot-toast";
+import { Routes } from "react-router-dom";
+
 const App = () => {
-  return <div>app</div>;
+  return (
+    <>
+      <Toaster />
+      <Routes></Routes>
+    </>
+  );
 };
 
 export default App;
